b746a2716f0d94b38913293ddd8ce7735fa0221b
Jimmylan <jimmylan.srq@gmail.com>
2023-07-25 18:43:56 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'andromed_sample_java_2023_rouen';
+}
